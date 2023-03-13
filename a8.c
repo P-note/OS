ada3fde2e89f50df48d2194780c3c25162b3b2e4
P-note <65222580+P-note@users.noreply.github.com>
2023-03-14 03:22:01 +0900

    Add files via upload

---
+++ a8.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void){
+    int fd[2];
+    int rc, rc2;
+    char* message;
+    if(pipe(fd) < 0){
+        printf("pipe error mate");
+        return -1;
+    }
+    
+    rc = fork();
+
+    if(rc < 0){
+        fprintf(stderr,"fork error mate");
+        exit(1);
+    }
+    else if(rc == 0){
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        printf("Transfer");
+    }
+
+    rc2=fork();
+    if(rc2 < 0){
+        fprintf(stderr,"fork2 error mate");
+        exit(1);
+    }
+    else if(rc2==0){
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        scanf("%s", &message);
+        printf(message);
+    }
+}
